2b617d2b76dc4ba5d2aa2ec9556df3953107cff0
Wyatt Erp <henderson.wyatt95@gmail.com>
2020-08-23 13:00:47 -0400

    Jquerey Test

---
+++ script.js
@@ -0,0 +1,12 @@
+var breakpoint = 991;
+var bgHeight = function() {
+    $('#bg').css('height', $(window).height() + 'px');
+}; bgHeight();
+var windowWidth = $(window).height();
+$(window).on('resize', function() {
+    if ((($(this).width() <= breakpoint) && ($(this).width() != windowWidth))
+        || ($(this).width() > breakpoint)) {
+        bgHeight();
+    }
+    windowWidth = $(window).width();
+});
